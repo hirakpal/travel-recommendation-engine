b8c3c335ed220a036696cb7c4967e1322a66c81a
hirakpal <60995278+hirakpal@users.noreply.github.com>
2026-08-08 22:17:41 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
 from setuptools import setup, find_packages
-
+from src.database.repository import HotelRepository
+from src.database.repository import ActivityRepository
+from src.database.repository import RestaurantRepository
 setup(
-    name="travel-planner-ai,
+    name="travel-recommendation-engine",
     version="0.1.0",
     description="Multi-agent AI travel recommendations",
     author="Your Name",
@@ -12,7 +14,9 @@ setup(
         "fastapi",
         "uvicorn",
         "pydantic",
-        "anthropic",
+        "openapi",
         "python-dotenv",
+        "sqlalchemy",
+        "psycopg2-binary",
     ],
 )
